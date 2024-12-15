af10bbf9cf093c4bcc7bce19c6a576cd3ee31d38
toinux <toinux@gmail.com>
2024-12-15 15:11:23 +0100

    add frood

---
+++ keyboards/crkbd/rev1/keymaps/frood/rules.mk
@@ -0,0 +1,11 @@
+CONVERT_TO        = rp2040_ce
+MOUSEKEY_ENABLE   = yes    # Mouse keys
+RGBLIGHT_ENABLE   = no
+RGB_MATRIX_ENABLE = yes
+OLED_ENABLE       = yes
+LTO_ENABLE        = yes
+BOOTLOADER        = atmel-dfu
+CAPS_WORD_ENABLE  = yes
+DYNAMIC_MACRO_ENABLE = yes
+
+SRC += ./oled.c ./rgb.c
